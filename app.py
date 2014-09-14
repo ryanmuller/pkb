a02aa96fbc9e5820a1398dba4b2ebf43e3ccd476
ryanmuller <cognitionmachine@gmail.com>
2014-09-13 22:30:28 -0400

    scrape pdfs

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from sys import argv
-
+import os
+import os.path
 import bottle
 from bottle import route, static_file
 from newspaper import Article
+import urllib2
+import shutil
+import hashlib
+import StringIO
+import re
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
 
 @route('/')
 def root():
@@ -18,8 +28,34 @@ def page(name):
 def asset(filepath):
     return static_file(filepath, root="./static/")
 
+@route('/scrape-pdf/<url:path>')
+def scrape_pdf(url):
+    m = hashlib.md5()
+    m.update(url)
+    f = "./tmp/{fname}.pdf".format(fname=m.hexdigest())
+
+    if not os.path.isfile(f):
+        req = urllib2.urlopen(url)
+        with open(f, 'wb') as fp:
+            shutil.copyfileobj(req, fp)
+
+    with open(f, 'rb') as fp:
+        outfp = StringIO.StringIO()
+        rsrcmgr = PDFResourceManager()
+        interpreter = PDFPageInterpreter(rsrcmgr, TextConverter(rsrcmgr, outfp, codec="utf-8", laparams=LAParams()))
+        for page in PDFPage.get_pages(fp):
+            interpreter.process_page(page)
+        out = outfp.getvalue()
+        outfp.close()
+        return { 'url': url,
+                 'content': out }
+
 @route('/scrape/<url:path>')
 def scrape(url):
+    pdf_pattern = re.compile('\.pdf$')
+    if pdf_pattern.search(url):
+        return scrape_pdf(url)
+
     a = Article(url, keep_article_html=True)
     a.download()
     a.parse()
