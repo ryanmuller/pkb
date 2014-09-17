1b34fc80f384f75da67d69f7e59b7f829962e2b5
ryanmuller <cognitionmachine@gmail.com>
2014-09-16 23:29:47 -0400

    add heroku bookmarklet

---
+++ static/js/bookmarklet_heroku_raw.js
@@ -0,0 +1,30 @@
+function getHTMLOfSelection () {
+  var range;
+  if (document.selection && document.selection.createRange) {
+    range = document.selection.createRange();
+    return range.htmlText;
+  }
+  else if (window.getSelection) {
+    var selection = window.getSelection();
+    if (selection.rangeCount > 0) {
+      range = selection.getRangeAt(0);
+      var clonedSelection = range.cloneContents();
+      var div = document.createElement('div');
+      div.appendChild(clonedSelection);
+      return div.innerHTML;
+    }
+    else {
+      return '';
+    }
+  }
+  else {
+    return '';
+  }
+}
+
+var title = encodeURIComponent(document.title.substring(0,64)),
+    pages = encodeURIComponent(prompt("Add to pages")),
+    source = encodeURIComponent(location.href),
+    extract = encodeURIComponent(getHTMLOfSelection());
+
+location.href = "http://pkb.herokuapp.com/import?title="+title+"&pages="+pages+"&source="+source+"&extract="+extract;
